95573b13fd5fb9c0e7a8e371400494df5de16b22
RafaelaTerra <rafaelaterra@gec.inatel.br>
2018-12-19 14:11:27 -0200

    BOTAO CANCELAR

--- frontend/src/analyze/analyzeForm.jsx
+++ frontend/src/analyze/analyzeForm.jsx
@@ -29,7 +29,7 @@ class AnalyzeForm extends Component {
                     <button type='submit' className={`btn btn-${this.props.submitClass}`}>
                         {this.props.submitLabel}
                     </button>
-                    <button type='button' className=' btn btn-default' onClick={this.props.init}>Cancel</button>
+                    <button type='button' className=' btn btn-default' onClick={this.props.init}>Cancelar</button>
                 </div>
             </form>
         )
